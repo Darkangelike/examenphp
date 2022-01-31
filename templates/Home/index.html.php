8fb396d3db61f34bad03068117f86e849445d281
Isabelle <isabelle.tieu@gmail.com>
2022-01-31 13:10:22 +0100

    Initial commit

---
+++ templates/Home/index.html.php
@@ -0,0 +1,15 @@
+<div class="text-center">
+    
+    
+    <h1>Welcome page of the base Framework</h1>
+    
+        <div class="border border-dark">
+    
+        <p>Additional documentation here</p>
+        
+        </div>
+    
+    
+    <a href="" class="btn btn-primary">Go to the exam</a>
+
+</div>
